d646615d515b498c6b9f3b2c2bdf9d9e295e9c11
accepter <acceptancy@gmail.com>
2011-11-24 14:42:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $dsn = "mysql:host=10.194.111.8;dbname=db_f30db62b";
 $db = new PDO($dsn, 'user_f30db62b', 'JBlIGgE@^1f1LN');
-$count = $db-＞exec("INSERT INTO foo SET name = 'heiyeluren',gender='男',time=NOW()");
+$count = $db->exec("INSERT INTO foo SET name = 'heiyeluren',gender='男',time=NOW()");
 echo $count;
 
 ?> 
