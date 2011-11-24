90b3ea5a2b94e1a1e6ecbef295f841bbb1a55dfa
accepter <acceptancy@gmail.com>
2011-11-24 14:20:24 +0800

    pod test

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
-<?php
-echo "OOK";
-?>
+＜?php
+　$dsn = "mysql:host=10.194.111.8;dbname=db_f30db62b";
+　$db = new PDO($dsn, user_f30db62b,JBlIGgE@^1f1LN );
+　$count = $db-＞exec("INSERT INTO test SET name = heiyeluren,gender=男,time=NOW()");
+　echo $count;
+　$db = null;
+?＞ 
