354a59afa13748524f058a4d20caa4c3f59ac3fb
accepter <acceptancy@gmail.com>
2011-11-24 14:35:15 +0800

    back original

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 ＜?php
-　$dsn = "mysql:host=10.194.111.8;dbname=db_f30db62b";
-　$db = new PDO($dsn, user_f30db62b,JBlIGgE@^1f1LN );
-　$count = $db-＞exec("INSERT INTO test SET name = heiyeluren,gender=男,time=NOW()");
+　
+　$count = "123";
 　echo $count;
-　$db = null;
+　
 ?＞ 
