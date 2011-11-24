16ff2b9b06cdb5fb55e2db0118abe3f3924ccec1
accepter <acceptancy@gmail.com>
2011-11-24 14:40:16 +0800

    PDO test

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-＜?php
-　
-　$count = "123";
-　echo $count;
-　
-?＞ 
+<?php
+$dsn = "mysql:host=10.194.111.8;dbname=db_f30db62b";
+$db = new PDO($dsn, 'user_f30db62b', 'JBlIGgE@^1f1LN');
+$count = $db-＞exec("INSERT INTO foo SET name = 'heiyeluren',gender='男',time=NOW()");
+echo $count;
+
+?> 
